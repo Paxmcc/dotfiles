c6583a08ce6243ae6b05261c2f3a9e007b6017dc
Pax McCausland <paxmccausland@gmail.com>
2026-04-18 13:17:55 +1000

    fix picom.sh to install base-devel so the build works properly

--- all/picom.sh
+++ all/picom.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+sudo pacman -Syy base-devel --needed --noconfirm
+
 cd ~
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
+cd ~
+rm -rf yay-bin
 
 yay -Syy picom-git --needed --noconfirm
 
